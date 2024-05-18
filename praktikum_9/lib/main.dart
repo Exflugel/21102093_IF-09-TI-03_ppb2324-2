6a3a377f884806be677adf8726e1789d0724a9de
azzamarib13@gmail.com <21102093@ittelkom-pwt.ac.id>
2024-05-18 20:51:03 +0700

    firebase Authentification

--- praktikum_9/lib/main.dart
+++ praktikum_9/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:praktikum_9/bloc/login/login_cubit.dart';
 import 'package:praktikum_9/bloc/register/register_cubit.dart';
+import 'package:praktikum_9/ui/home_screen.dart';
+import 'package:praktikum_9/ui/login.dart';
 import 'package:praktikum_9/ui/splash.dart';
 import 'package:praktikum_9/utils/routes.dart';
 import 'firebase_options.dart';
@@ -28,7 +31,23 @@ class MyApp extends StatelessWidget {
  debugShowCheckedModeBanner: false,
  navigatorKey: NAV_KEY,
  onGenerateRoute: generateRoute,
- home: SplashScreen(),
+ home: StreamBuilder<User?>(
+  stream: FirebaseAuth.instance.authStateChanges(),
+  builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const CircularProgressIndicator();
+    } else if (snapshot.hasData) {
+      return HomeScreen();
+    } else if (snapshot.hasError) {
+      return const Center(
+        child: Text('Something went wrong'),
+      );
+    } else {
+      return const LoginScreen();
+    }
+  },
+),
+
  ),
  );
  }
